55d24e5a89c6fa14f9ef56e8ba73ea62325a404d
Muhammad Sameer <154794709+Tec-Sam@users.noreply.github.com>
2024-05-01 01:43:40 +0500

    js added in about

--- js/main.js
+++ js/main.js
@@ -54,6 +54,23 @@
         allowParentLinks: true
     });
 
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+    });
+       
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
     /*------------------
 		Hero Slider
 	--------------------*/
